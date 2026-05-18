9d72ba41396a97dc0d3835261c4aec8b733f2794
Iklnch <igork3142@gmail.com>
2026-05-18 20:28:49 +0200

    sys!: updating apps

--- hello_app_fastapi/main.py
+++ hello_app_fastapi/main.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 
 from hello_app_fastapi.routers import user_data
 
-app = FastAPI(root_path="/fastapi")
+app = FastAPI()
 app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 #Rendering home page
--- hello_app_flask/app.py
+++ hello_app_flask/app.py
@@ -6,12 +6,6 @@ from werkzeug.wrappers import Response
 
 # Using blueprints to organize the application into components
 app = Flask(__name__)
-app.wsgi_app = DispatcherMiddleware(
-    Response("Not Found", status=404),
-    {
-        "/flask": app.wsgi_app
-    }
-)
 app.register_blueprint(index.index_page)
 app.register_blueprint(user_data.user_data_page)
 
