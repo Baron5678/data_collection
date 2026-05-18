fef23ab41ca433005bb7476fbfeddd57c584c91b
Iklnch <igork3142@gmail.com>
2026-05-18 19:33:21 +0200

    sys!: updating configs

--- celery_app/app.py
+++ celery_app/app.py
@@ -1,7 +1,7 @@
 from celery import Celery
 # Celery application configuration
 # Redis is used as both broker and result backend
-app = Celery('data_collection', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+app = Celery('data_collection', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
 app.conf.broker_transport_options = {'visibility_timeout': 3600,
                                      'retry_policy': { 'timeout': 5.0 }
